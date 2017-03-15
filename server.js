e0412b6a6fc6bbbc8401ff22f7a32caf8881ba48
= <hmgaar@gmail.com>
2017-03-14 20:17:22 -0400

    created express server

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var path = require('path');
+
+// var users = require('./routes/users');
+var port = process.env.PORT || 5000
+var app = express();
+
+// // expose node_modules to client app
+// app.use(express.static(__dirname));
+app.use(express.static(__dirname + "/node_modules"));
+app.use(express.static(path.join(__dirname)));
+// app.use(express.static(path.join(__dirname, 'src/app')));
+app.get('/', function(req, res) {
+        res.sendfile('./index.html'); // load the single view file (angular will handle the page changes on the front-end)
+    });
+app.listen(port);
+module.exports = app;
